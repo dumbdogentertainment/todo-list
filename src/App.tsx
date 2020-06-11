26adfb66c63f0181acd6b7c1c4bd644766a8510d
Robert <packleader@dumbdogentertainment.com>
2020-06-10 22:16:14 -0500

    -- removed default files added by project init and finished part 1 of tutorial;

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { TodoListItem } from "./TodoListItem";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const todos: Array<Todo> = [
+    {text: "Walk the dog", complete: true},
+    {text: "Write app", complete: false}
+];
+
+const App: React.FC = () => {
+    return (
+        <React.Fragment>
+            <TodoListItem todo={todos[0]} />
+            <TodoListItem todo={todos[1]} />
+        </React.Fragment>
+    );
+};
 
 export default App;
